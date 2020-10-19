522e3b23fc9d1d818d9ce6717480519e4cfafe6c
Aleksei Androsov <aandrosov@yandex-team.ru>
2020-10-19 17:39:54 +0300

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
+'use strict';
+
 module.exports = {
     env: {
         es6: true,
-        node: true
+        node: true,
     },
     extends: 'eslint:recommended',
     parserOptions: {
-        ecmaVersion: 2015
+        ecmaVersion: 2015,
     },
     rules: {
         'array-bracket-spacing': [ 'error', 'always' ],
@@ -18,5 +20,5 @@ module.exports = {
         semi: [ 'error', 'always' ],
         strict: [ 'error', 'global' ],
         'template-curly-spacing': [ 'error', 'always' ],
-    }
+    },
 };
